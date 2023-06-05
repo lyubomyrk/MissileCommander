b944efc67499c116a26c15c48aead9c86b6e837e
Lyubomyr Kryshtanovskiy <lyubomyr05@gmail.com>
2023-06-04 17:31:07 -0700

    Add project files.

---
+++ Source/MissileCommander.cpp
@@ -0,0 +1,12 @@
+﻿// MissileCommander.cpp : Defines the entry point for the application.
+//
+
+#include "MissileCommander.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
