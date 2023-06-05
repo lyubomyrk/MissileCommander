26f600a37293dfbe1ac17f61355157df4031ee78
Lyubomyr Kryshtanovskiy <lyubomyr05@gmail.com>
2023-06-04 19:09:31 -0700

    Explosions are now dynamic, they grow and shrink.

--- Source/MissileCommander.cpp
+++ Source/MissileCommander.cpp
@@ -14,6 +14,10 @@ struct Missile {
 	Vector2 vel;
 	Vector2 dest;
 	bool disassembly;
+	float radius_dir;
+	float explosion_radius;
+	float max_radius;
+	float growth_speed;
 };
 
 int main()
@@ -41,42 +45,55 @@ int main()
 			Vector2 missile_vel = Vector2Scale(Vector2Normalize(cannon_dest_diff), missile_speed);
 			Vector2 missile_pos = { cannon.x + cannon_offset.x, cannon.y };
 			Missile* new_missile = new Missile{ missile_pos, missile_vel, missile_dest, false };
+			new_missile->max_radius = 25;
 			missiles.push_back(new_missile);
 		}
 
+
 		for (auto& missile : missiles) {
-			if (!missile->disassembly)
+			if (!missile->disassembly) {
 				missile->pos = Vector2Add(missile->pos, Vector2Scale(missile->vel, dt));
-			if (Vector2Distance(missile->pos, missile->dest) <= 5.) {
-				missile->disassembly = true;
+				// BUG if missiles dont explode check here
+				if (Vector2Distance(missile->pos, missile->dest) <= 5.) {
+					missile->disassembly = true;
+					missile->explosion_radius = 0.1;
+					missile->radius_dir = 1;
+					missile->growth_speed = 10;
+				}
+			}
+			if (missile->disassembly) {
+				missile->explosion_radius = missile->explosion_radius + missile->radius_dir * missile->growth_speed * dt;
+				if (missile->explosion_radius >= missile->max_radius) {
+					missile->radius_dir = -1.;
+				}
 			}
 		}
 
 		// @formatter off
 		BeginDrawing();
-			ClearBackground(BLACK);
-			DrawFPS(10, 10);
-			// draw environment
-			DrawRectangleRec(ground, GREEN);
-			// draw cannons
-			DrawRectangleRec(cannon, BLUE);
-			DrawLine(ScreenWidth / 2, 0, ScreenWidth / 2, ScreenHeight, PURPLE);
+		ClearBackground(BLACK);
+		DrawFPS(10, 10);
+		// draw environment
+		DrawRectangleRec(ground, GREEN);
+		// draw cannons
+		DrawRectangleRec(cannon, BLUE);
+		DrawLine(ScreenWidth / 2, 0, ScreenWidth / 2, ScreenHeight, PURPLE);
 
-			// draw crosshair
-			DrawLine(mouse.x - 5, mouse.y - 5, mouse.x + 5, mouse.y + 5, RED);
-			DrawLine(mouse.x + 5, mouse.y - 5, mouse.x - 5, mouse.y + 5, RED);
+		// draw crosshair
+		DrawLine(mouse.x - 5, mouse.y - 5, mouse.x + 5, mouse.y + 5, RED);
+		DrawLine(mouse.x + 5, mouse.y - 5, mouse.x - 5, mouse.y + 5, RED);
 
-			// draw missiles
-			for (auto& missile : missiles) {
-				if (missile->disassembly) {
-					DrawCircle(missile->pos.x, missile->pos.y, 5, WHITE);
-					missile->vel = { 0., 0, };
-					continue;
-				}
-				Vector2 line_start = { cannon.x + cannon_offset.x, cannon.y };
-				Vector2 line_end = { missile->pos.x, missile->pos.y };
-				DrawLineV(line_start, line_end, GREEN);
+		// draw missiles
+		for (auto& missile : missiles) {
+			if (missile->disassembly) {
+				DrawCircle(missile->pos.x, missile->pos.y, missile->explosion_radius, WHITE);
+				missile->vel = { 0., 0, };
+				continue;
 			}
+			Vector2 line_start = { cannon.x + cannon_offset.x, cannon.y };
+			Vector2 line_end = { missile->pos.x, missile->pos.y };
+			DrawLineV(line_start, line_end, GREEN);
+		}
 		EndDrawing();
 		// @formatter on
 	}
