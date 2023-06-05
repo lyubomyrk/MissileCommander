3c0c23e754b6c2ab5faf0baba86214cb0c1ca499
Lyubomyr Kryshtanovskiy <lyubomyr05@gmail.com>
2023-06-04 18:12:35 -0700

    Linked the raylib library

--- Source/MissileCommander.cpp
+++ Source/MissileCommander.cpp
@@ -2,11 +2,27 @@
 //
 
 #include "MissileCommander.h"
+#include "raylib.h"
+#include "raymath.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	const int ScreenWidth = 680;
+	const int ScreenHeight = 480;
+
+	InitWindow(ScreenWidth, ScreenHeight, "Missile Commander!");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawFPS(10, 10);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
